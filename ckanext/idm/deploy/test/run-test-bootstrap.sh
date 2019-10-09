e128e86c28b344b75c5f8d8f3faa7e526d6c6d6f
mewu <mewu@idmod.org>
2019-10-08 20:54:35 -0700

    add tests to cover backup/restore #59

--- ckanext/idm/deploy/test/run-test-bootstrap.sh
+++ ckanext/idm/deploy/test/run-test-bootstrap.sh
@@ -20,3 +20,48 @@ python bootstrap.py $apikey -f ../test/test_metadata.yaml -p testPassword --forc
 
 #import
 docker exec -i -w /home/ckan/src/ckan/ckanext/idm/deploy/data/ ckan python import.py $apikey import/dataset-fields-map-Caitlin.yaml import/datasets-Caitlin.csv --force
+
+#backup
+echo "remove old backup"
+rm -rf  /mnt/ckan/backup/$HOSTNAME/test/
+cd ../ops
+now=$(date +"%m_%d_%Y")
+echo "create new backup"
+bash volumes.sh backup test 2>&1 | tee -a /home/mewu/ckan/backup_$now.log
+backupfile=$(ls /mnt/ckan/backup/$HOSTNAME/test/ | grep '.tar.gz')
+backupfile="/mnt/ckan/backup/$HOSTNAME/test/"$backupfile
+
+#create new user
+username=$(uuidgen -t)
+echo "create user: "$username
+json=$(http --json POST http://$host:5000/api/3/action/user_create name=$username email=mewu@gmail.com password=12345678 Authorization:$apikey)
+echo $json
+userid=$(echo $json | jq '.result.id' | sed 's/"//g')
+json_result=$(http --json GET http://$host:5000/api/3/action/user_list q=$username)
+userid_result=$(echo $json_result | jq '.result[].id' | sed 's/"//g')
+[ -z "$userid_result" ] && echo "Error: User creation failed before restore" && exit 1
+
+#delete old user
+echo "delete user: mewu"
+http --json POST http://$host:5000/api/3/action/user_delete id=mewu Authorization:$apikey
+json_deleted=$(http --json GET http://$host:5000/api/3/action/user_list q=mewu)
+echo $json_deleted
+result_deleted=$(echo $json_deleted | jq '.result[].id' | sed 's/"//g')
+[ ! -z "$userid_deleted" ] && echo "Error: User deletion failed before restore" && exit 1
+#restore
+echo "restore from backup: "$backupfile
+echo "delete-all-data" | bash volumes.sh restore $backupfile  2>&1 | tee -a /home/mewu/ckan/restore_$now.log
+
+#check new user is no longer there
+echo "check user: "$username
+json_result=$(http --json GET http://$host:5000/api/3/action/user_list q=$username)
+echo $json_result
+userid_result=$(echo $json_result | jq '.result[].id' | sed 's/"//g')
+[ ! -z "$userid_result" ] && echo "Error: something wrong after restore" && exit 1
+
+#check deleted user is back
+echo "check user: mewu"
+json_deleted=$(http --json GET http://$host:5000/api/3/action/user_list q=mewu)
+echo $json_deleted
+result_deleted=$(echo $json_deleted | jq '.result[].id' | sed 's/"//g')
+[ -z "result_deleted" ] && echo "Error: deleted user not recover restore" && exit 1
--- ckanext/idm/deploy/test/test-deploy.sh
+++ ckanext/idm/deploy/test/test-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+webhookurl=https://outlook.office.com/webhook/ecfa8776-a22c-414f-975a-c1735464c14a@7cae4d00-e099-4686-9873-ba27c351d11e/IncomingWebhook/2fb34f43a558470c9644a3ba77374281/a70235de-2b93-4ad1-9b26-b7520d7ec585
 cd /home/mewu/ckan/ckan
 git reset --hard
 git pull --rebase
@@ -6,7 +7,21 @@ cd ckanext/idm/deploy
 docker-compose down -v
 chmod +x deploy.sh
 now=$(date +"%m_%d_%Y")
-./deploy.sh prod 2>&1 | tee -a /home/mewu/ckan/deploy_$now.log
+rm -rf /mnt/ckan/test/deploy/*.*
+./deploy.sh refresh-prod 2>&1 | tee -a /home/mewu/ckan/deploy_$now.log
 cd test
 chmod +x run-test-bootstrap.sh
-./run-test-bootstrap.sh
+./run-test-bootstrap.sh 2>&1 | tee -a /home/mewu/ckan/bootstrap_$now.log
+
+yes | cp -rf /home/mewu/ckan/*_$now.log /mnt/ckan/test/deploy
+logfile=file://P:/ckan/test/deploy
+
+err_deploy=$(cat /home/mewu/ckan/deploy_$now.log | grep Error)
+err_backup=$(cat /home/mewu/ckan/backup_$now.log | grep Error)
+err_restore=$(cat /home/mewu/ckan/restore_$now.log | grep Error)
+err_bootstrap=$(cat /home/mewu/ckan/bootstrap_$now.log | grep Error)
+
+curl -H "Content-Type: application/json" -d "{\"text\": \"Deployed Finished: see logs in ${logfile}\n ERROR FROM Log:\n $err_deploy\"}" $webhookurl
+curl -H "Content-Type: application/json" -d "{\"text\": \"Backup Finished: see logs in ${logfile}\n ERROR FROM Log:\n $err_backup\"}" $webhookurl
+curl -H "Content-Type: application/json" -d "{\"text\": \"Restore Finished: see logs in ${logfile}\n ERROR FROM Log:\n $err_restore\"}" $webhookurl
+curl -H "Content-Type: application/json" -d "{\"text\": \"Bootstrap Finished: see logs in ${logfile}\n ERROR FROM Log:\n $err_bootstrap\"}" $webhookurl
