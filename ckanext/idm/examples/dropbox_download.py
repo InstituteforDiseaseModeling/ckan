7834e29c9ad94f8d2976cba591cd81fb2f31715d
dlukacevic-idm <dlukacevic@idmod.org>
2019-12-16 18:01:35 -0800

    Fix scenario_2_download_dir_from_dropbox to add .zip file to the download path, cased on top Dropbox dir name. Make terminology consistent.

--- ckanext/idm/examples/dropbox_download.py
+++ ckanext/idm/examples/dropbox_download.py
@@ -5,17 +5,20 @@
 # Setup:
 # - Install a new Python 2.7 virtual environment
 # - pip install -r requirement.txt
-# - Find Data Catalog API key.
+# - Find Data Catalog API Key.
 #       + In Data Catalog click on user name (top right).
 #       + Look at the left bottom part of the page.
+#       + For example, 11111111-2222-3333-4444-555566667777
 # - Find Data Catalog dataset ID.
 #       + In Data Catalog open the dataset page and find ID in the browser URL (last part).
+#       + For example, in "http://data-catalog/dataset/pakistan-worldpop" dataset ID is "pakistan-worldpop".
 # - Generate Dropbox authentication token (scenario 2 only):
-#   Instruction: https://www.iperiusbackup.net/en/create-dropbox-app-get-authentication-token/
+#       + See instruction: https://www.iperiusbackup.net/en/create-dropbox-app-get-authentication-token/
+#       + For example, xl_4444444444x4333_11111111117777777
 #
 # Run:
 # - Usage:
-#       python dropbox_download.py DATA-CATALOG_API_GUID DATASET_ID [--token DROPBOX_TOKEN_GUID] [--dir-download]
+#       python dropbox_download.py {Data Catalog API Key} {Data Catalog dataset ID} [--token {Dropbox authentication token}] [--dir-download]
 # - For example (below guids are made-up):
 #       python dropbox_download.py 11111111-2222-3333-4444-555566667777 pakistan-worldpop --token xl_4444444444x4333_11111111117777777
 
@@ -47,7 +50,7 @@ def main(host_url, apikey, dataset_id, dropbox_token, dir_download):
     # Scenario 2a: Download a file from Dropbox
     dbx = dropbox.Dropbox(dropbox_token)
     download_path = tempfile.mkdtemp()
-    scenario_2_download_file_from_dropbox(dropbox_path, dbx, download_path, u'licence.txt')
+    scenario_2_download_file_from_dropbox(dbx, dropbox_path, download_path, u'licence.txt')
     print 'Downloaded file into {}'.format(download_path)
     #
     # YOUR CODE COULD GO HERE
@@ -55,7 +58,7 @@ def main(host_url, apikey, dataset_id, dropbox_token, dir_download):
 
     # Scenario 2b: Download the entire dir from Dropbox
     if dir_download:
-        metadata = scenario_2_download_dir_from_dropbox(dataset_id, dropbox_path, dbx, download_path)
+        metadata = scenario_2_download_dir_from_dropbox(dbx, dropbox_path, download_path)
         print 'Downloaded all files into {}'.format(download_path)
         print metadata
         #
@@ -78,13 +81,15 @@ def scenario_1_local_dropbox_path(dropbox_path):
     return dropbox_local_path
 
 
-def scenario_2_download_file_from_dropbox(dropbox_path, dbx, download_path, file_name):
+def scenario_2_download_file_from_dropbox(dbx, dropbox_path, download_path, file_name):
     download_file_path = os.path.join(download_path, file_name)
     dropbox_file_path = '{}/{}'.format(dropbox_path, file_name)
     dbx.files_download_to_file(download_file_path, dropbox_file_path)
 
 
-def scenario_2_download_dir_from_dropbox(dropbox_path, dbx, download_path):
+def scenario_2_download_dir_from_dropbox(dbx, dropbox_path, download_dir_path):
+    dir_name = '{}.zip'.format(os.path.basename(dropbox_path))
+    download_path = os.path.join(download_dir_path, dir_name)
     metadata = dbx.files_download_zip_to_file(download_path, dropbox_path)
 
     return metadata
@@ -94,7 +99,7 @@ if __name__ == '__main__':
     example = 'python dropbox_download.py DATA-CATALOG_API_GUID DATASET_ID [--token DROPBOX_TOKEN_GUID] [--dir-download]'
     parser = argparse.ArgumentParser(example)
 
-    parser.add_argument('apikey', default=None, help='Data Catalog API key.')
+    parser.add_argument('apikey', default=None, help='Data Catalog API Key.')
     parser.add_argument('dataset_id', default='pakistan-worldpop', help='Data Catalog dataset ID. ')
     parser.add_argument('--token', dest='dropbox_token', default=None, help='Dropbox app authentication token. ')
     parser.add_argument('--dir-download', dest='dir_download', action='store_true', default=False, help='Download the entire dataset dir.')
